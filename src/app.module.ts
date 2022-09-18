4c6ffe96dda3645cad5442e1908b2485fc9a457b
Carlos Córdova <46693089+carloscdev@users.noreply.github.com>
2022-09-17 22:38:26 -0500

    Revert "Develop leinadpy"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
-    CatsModule,
-  ],
+  imports: [],
 })
 export class AppModule {}
