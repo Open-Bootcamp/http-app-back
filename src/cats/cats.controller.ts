a626cb936182199708e1495e0ee1b1eaa685464e
Daniel <leinadpy@gmail.com>
2022-09-17 23:23:01 -0400

    ConfigModule y MongooseModule configurados, entity modificada de acuerdo al Schema

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,18 +1,17 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CatsService } from './cats.service';
-import { Cat } from './entities/cat.entity';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
-  getAllCats(): Cat[] {
+  getAllCats() {
     return this.catsService.findAll();
   }
 
   @Get(':code')
-  getCatByCode(@Param('code', ParseIntPipe) code: number): Cat {
+  getCatByCode(@Param('code', ParseIntPipe) code: number) {
     return this.catsService.findOneByCode(code);
   }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,16 +1,24 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import * as catData from './../../catData.json';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+// import * as catData from './../../catData.json';
+import { Cat } from './entities/cat.entity';
 
 @Injectable()
 export class CatsService {
-  private cats = catData;
+  // private cats = catData;
+
+  constructor(
+    @InjectModel(Cat.name)
+    private readonly catModel: Model<Cat>,
+  ) {}
 
   findAll() {
-    return this.cats;
+    return this.catModel.find();
   }
 
-  findOneByCode(code: number) {
-    const cat = this.cats.find((cat) => cat.code === code);
+  async findOneByCode(code: number) {
+    const cat = await this.catModel.findOne({ code });
     if (!cat) throw new NotFoundException(`Status Code ${code} not found`);
     return cat;
   }
